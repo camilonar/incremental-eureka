6a872ec809d03ced66621dd28fb128e1d0c0b0c5
Camilo <camilonar09@gmail.com>
2018-02-27 00:18:48 -0500

    Módulo de pruebas v1
    Versión inicial del módulo independiente para las pruebas de la red
    neuronal

--- incremental.py
+++ incremental.py
@@ -5,6 +5,7 @@ import utils as ut
 import numpy as np
 import nnet as nn
 import core as co
+import train_conf as conf
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -54,21 +55,6 @@ def obtener_datasetSatelite():
 def split_dataset(x, y, offset, size):
     return x[offset:size], y[offset:size]
 
-
-# Test the neural network against the training dataset and a test dataset to review performance
-def probar_dataset(sess, x_input_v, y_input_v, x_test_v, y_test_v):
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    test_result = 1.0 - sess.run(accuracy, feed_dict={x: x_test_v, y_: y_test_v})
-    train_result = 1.0 - sess.run(accuracy, feed_dict={x: x_input_v, y_: y_input_v})
-
-    print("Resultados Test: " + str(test_result))
-    print("Resultados Training: " + str(train_result))
-
-    return test_result, train_result
-
-
 # Aux. function for configuring parsing options
 def config_parser():
     parser = OptionParser()
@@ -104,9 +90,6 @@ parser = config_parser()
 archivo = open("resultados.txt", 'w')
 archivo.write("type;seed;first_batch;second_batch\n")
 
-tf.reset_default_graph()
-tf.set_random_seed(59)
-
 #trainX, trainY, x_test, y_test = obtener_datasetSatelite()
 trainX, trainY, x_test, y_test = obtener_datasetLetterRe()
 
@@ -132,12 +115,9 @@ trainX_B2, trainY_B2 = split_dataset(trainX, trainY, size_batch,
 print("Size first batch : ", trainX_B1.shape)
 print("Size second batch : ", trainX_B2.shape)
 
-# definimos los pesos  (que tan importante es ) en 1
-x_pesosDefault = np.full((size_batch, n_output), 1)
-
 for j in range(options.iterations):
     seeds = np.append(seeds, random.randint(1, 100))
-
+seeds = [5]
 for k in range(3):
     # Se salta la prueba si corresponde con las opciones
     if skip_test(k, options):
@@ -151,52 +131,29 @@ for k in range(3):
     for iteration in range(options.iterations):
         tf.reset_default_graph()
         tf.set_random_seed(seeds[iteration])  # fijamos un valor para la semilla de numeros aleatorios de tensor
-        x_pesos = tf.placeholder(tf.float32, [None, n_output])
         x = tf.placeholder(tf.float32, [None, input_layer])  # representa la entrada
-        y_ = tf.placeholder(tf.float32, [None, n_output])  # representa la salida deseada
         y = nn.create_neural_net(x,[input_layer,n_hidden,n_hidden_2,n_output], act=tf.nn.tanh)
-        mse = tf.reduce_mean(tf.square(y - y_) * x_pesos)
-        train_step = tf.train.AdamOptimizer(0.004).minimize(mse)
+        configuration = conf.TrainingConfiguration(tf, x, y, x_test, y_test)
         archivo.write(str(k) + ";" + str(seeds[iteration]) + ';')
         init = tf.global_variables_initializer()
         sess = tf.InteractiveSession()
         sess.run(init)
-        for i in range(500):
-            _, c = sess.run([train_step, mse], feed_dict={x: trainX_B1, y_: trainY_B1, x_pesos: x_pesosDefault})
-            if i % 10 == 0:
-                print(i * 100 / 150, " val-> OK")
-        print("\n:::PROBANDO:::::::::::\n")
-        test_results, _ = probar_dataset(sess, trainX_B1, trainY_B1, x_test, y_test)
+        #Se entrena el primer lote
+        test_results = configuration.run_basic_test(tf, sess, 500, trainX_B1, trainY_B1)
         archivo.write(str(test_results) + ';')
         if (k == COMPLETO):
             archivo.write('\n')
             continue
         delta_f = 0.2
         delta_c = 0.55
-        x_pesosConRep = np.full((len(trainX_B2), n_output), 1)
-        if (k == REPRESENTANTES):
-            _, frontera_x, frontera_y = co.get_fronteras(sess, x, y, y_, trainX_B1, trainY_B1, delta_f, 250)
-            _, centros_x, centros_y = co.get_centros(sess, x, y, y_, trainX_B1, trainY_B1, delta_c, 20)
-            x_pesosConRep = np.append(x_pesosConRep, np.full((len(frontera_x) + len(centros_x), n_output), 3), axis=0)
-            trainX_B2_aux = np.concatenate((trainX_B2, np.asanyarray(frontera_x)), axis=0)
-            trainX_B2_aux = np.concatenate((trainX_B2_aux, np.asanyarray(centros_x)), axis=0)
-            trainY_B2_aux = np.concatenate((trainY_B2, np.asanyarray(frontera_y)), axis=0)
-            trainY_B2_aux = np.concatenate((trainY_B2_aux, np.asanyarray(centros_y)), axis=0)
-            print("shape trainX ", trainX_B2_aux.shape)
-            print("shape trainY ", trainY_B2_aux.shape)
-            print("shape fronteraX ", np.asanyarray(frontera_x).shape)
-            print("shape fronteraY ", np.asanyarray(frontera_y).shape)
-        else:
-            trainX_B2_aux = np.copy(trainX_B2)
-            trainY_B2_aux = np.copy(trainY_B2)
         print("\n ::::::ENTRENANDO SEGUNDO LOTE CON :::::::\n")
-
-        for i in range(500):  # gradiente descendente estocastico
-            _, c = sess.run([train_step, mse], feed_dict={x: trainX_B2_aux, y_: trainY_B2_aux, x_pesos: x_pesosConRep})
-            if i % 500 == 0:
-                print('OK')
-        test_results, _ = probar_dataset(sess, trainX_B2, trainY_B2, x_test, y_test)
-        probar_dataset(sess, trainX_B1, trainY_B1, x_test, y_test)
+        #Se entrena el segundo lote
+        if (k == REPRESENTANTES):
+            _, frontera_x, frontera_y = co.get_fronteras(sess, configuration.x, configuration.y, configuration.y_, trainX_B1, trainY_B1, delta_f, 250)
+            _, centros_x, centros_y = co.get_centros(sess, configuration.x, configuration.y, configuration.y_, trainX_B1, trainY_B1, delta_c, 20)
+            test_results = configuration.run_prototypes_test(tf, sess, 500, trainX_B2, trainY_B2, frontera_x, frontera_y, centros_x, centros_y)
+        else:
+            test_results = configuration.run_basic_test(tf, sess, 500, trainX_B2, trainY_B2)
         archivo.write(str(test_results) + '\n')
         sess.close()
 
