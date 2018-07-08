c642f1fd9275432ba7fe2bbded3accb2eefdc7cf
Unknown <camilonar09@gmail.com>
2018-07-07 21:57:40 -0500

    Versión inicial LeCun
    Versión inicial de la CNN para MNIST, sin revisar funciones de activación o realización de summaries

---
+++ train_conf_v2.py
@@ -0,0 +1,66 @@
+"""
+Module for the configuration of training that is going to be used.
+There are two clases:
+    1. GeneralConfig, which has the configuration data for the whole training (including the incremental learning)
+    2. TrainConfig, which has the configuration data for a specific increment of data
+"""
+
+
+class TrainConfig:
+    """
+    Configuration for training based on a batch of data. This is used for a specific increment of data
+    """
+
+    def __init__(self, epochs: int, batch_size=100):
+        """
+        Creates a TrainConfig object
+        :param epochs: number of epochs for the training
+        :param batch_size: the sizes of the batches that are going to be used in training. This is different from the
+        number of instances of each incremental training. E.g. An incremental training may be of a total of 1000
+        samples using batches with batch_size 100
+        """
+        self.epochs = epochs
+        self.batch_size = batch_size
+
+
+class GeneralConfig:
+    """
+    General configuration that it's used for the whole training. E.g. if the training is composed of 5 different batches
+    of data, then this part of the configuration is used for ALL of them
+    """
+
+    def __init__(self, learn_rate: float, momentum: float, reg: float, dropout=0.5,
+                 summary_interval=100, eval_interval=200, config_name='default', model_name='dataset_default'):
+        """
+        Creates a GeneralConfig object
+        :param learn_rate: the learning rate to be used in the training
+        :param momentum: the momentum to be used in the training
+        :param reg: the regularization measure to be used in training
+        :param dropout: the dropout probability to be used
+        :param summary_interval: the interval of epochs at which the summaries are going to be performed
+        :param eval_interval: the interval of epochs at which the evaluations are going to be performed. Must be an
+        integer multiple of summary_interval
+        :param config_name: a descriptive name for the training configuration
+        :param model_name: a descriptive name for the model
+        """
+        self.learn_rate = learn_rate
+        self.momentum = momentum
+        self.reg = reg
+        self.dropout = dropout
+        self.summary_interval = summary_interval
+        self.eval_interval = eval_interval
+        self.config_name = config_name
+        self.model_name = model_name
+        self.train_configurations = []  # It stores the configurations for each mega batch of training data
+        # model_name = 'vgg_16'  # choose model
+        # model = staticmethod(globals()[model_name])  # gets model by name
+
+    def add_train_conf(self, train_conf: TrainConfig):
+        """
+        Adds a TrainConfig object to the list of mega-batch-specific configurations. The objects must be added in
+        the same order as how they are going to be used (i.e. the configuration for the first batch must be added first,
+        for the second batch must be added second, ...)
+        :param train_conf: a configuration for a mega batch of data
+        :return: None
+        """
+        self.train_configurations.append(train_conf)
