35068292c28c628a7b5a03deb7a4b7b162152ee9
unknown <david-gomez12@hotmail.com>
2018-08-11 01:21:46 -0500

    add alexnet

--- models.py
+++ models.py
@@ -91,16 +91,24 @@ class VGGNet(Network):
          .fc(101, name='fc8')
          .softmax(name='prob'))
 
-
 # TODO: agregar líneas necesarias para los summaries
-# TODO: modificar la red para adaptarla a los valores de los artículos
-class AlexsNet(Network):
+#alexnet :http://vision.stanford.edu/teaching/cs231b_spring1415/slides/alexnet_tugce_kyunghee.pdf
+#TODO: EL NUMERO DE CLASES DEBE SER UNA VARIABLE ?
+
+class AlexNet(Network):
     def setup(self):
-        """
-        Creates a Neural Net with a Alex's Net architecture. The input data must have been previously set in
-         the constructor of the object as 'data'.
-         E.g.:
-            net = AlexsNet({'data': input_tensor})
-        :return: None
-        """
-        pass
+        (self.feed('data')
+         .conv(11, 11, 96, 4, 4, padding='VALID', name='conv1')
+         .lrn(2, 2e-05, 0.75, name='norm1')
+         .max_pool(3, 3, 2, 2, padding='VALID', name='pool1')
+         .conv(5, 5, 256, 1, 1, group=2, name='conv2')
+         .lrn(2, 2e-05, 0.75, name='norm2')
+         .max_pool(3, 3, 2, 2, padding='VALID', name='pool2')
+         .conv(3, 3, 384, 1, 1, name='conv3')
+         .conv(3, 3, 384, 1, 1, group=2, name='conv4')
+         .conv(3, 3, 256, 1, 1, group=2, name='conv5')
+         .fc(4096, name='fc6')
+         .fc(4096, name='fc7')
+         .fc(10, relu=False, name='fc8')
+         .softmax(name='prob'))
+
