7f5390c6dc58981581c92c3462a1699b677ed387
unknown <david-gomez12@hotmail.com>
2018-08-20 15:30:10 -0500

    cifar data with images normalized

--- input/cifar_data.py
+++ input/cifar_data.py
@@ -39,6 +39,7 @@ class CifarData(Data):
         pass
 
     def __build_generic_data_tensor(self, filename, shuffle, augmentation, testing, skip_count=0):
+        
         def parser(serialized_example):
             """Parses a single tf.Example into image and label tensors."""
             features = tf.parse_single_example(
@@ -48,15 +49,20 @@ class CifarData(Data):
                     'label': tf.FixedLenFeature([], tf.int64)
                 })
 
-            image = tf.decode_raw(features['image'], tf.uint8)
+            image = tf.decode_raw(features['image'], tf.uint8) 
             image.set_shape([3 * self.IMAGE_HEIGHT * self.IMAGE_WIDTH])
 
             # Reshape from [depth * height * width] to [depth, height, width].
             image = tf.cast(
                 tf.transpose(tf.reshape(image, [3, self.IMAGE_HEIGHT, self.IMAGE_WIDTH]), [1, 2, 0]),
                 tf.float32)
-            label = tf.cast(features['label'], tf.int32)
+            
+            image = tf.image.convert_image_dtype(image,
+                                                        dtype=tf.float32,
+                                                        saturate=True) * (1 / 255.0)
 
+            label = tf.cast(features['label'], tf.int32) 
+            label = tf.one_hot(label, depth=self.NUMBER_OF_CLASSES)
             # TODO: preprocessing custom
 
             return image, label
