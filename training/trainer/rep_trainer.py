c635232f580022004931b98b0129e526fc6e555e
Unknown <camilonar09@gmail.com>
2019-02-25 20:53:20 -0500

    FIX: memory leak, y UPDATE: recalcular pesos y métricas v1
    Se arregló una fuga de memoria en RepTrainer, debido a referencias obsoletas a arrays que ya no se usaban. También se agregó el recálculo de métricas y pesos basado en proporción de representantes.

--- training/trainer/rep_trainer.py
+++ training/trainer/rep_trainer.py
@@ -22,13 +22,14 @@ class RepresentativesTrainer(Trainer):
         super().__init__(config, model, pipeline, tensor_x, tensor_y, tester=tester, checkpoint=checkpoint)
 
         self.representatives = [[] for _ in range(model.get_output().shape[1])]
+        self.class_count = [0 for _ in range(model.get_output().shape[1])]
         self.weights = tf.placeholder(tf.float32, [None])
 
         self.buffered_reps = []
 
-        self.presel = 10  # Number of preselected samples
-        self.rep_per_batch = 10  # Number of representatives that are passed in each batch
-        self.r = 20  # Maximum number of representatives per class
+        self.presel = 18  # Number of preselected samples
+        self.rep_per_batch = 18  # Number of representatives that are passed in each batch
+        self.rep_per_class = 25  # Maximum number of representatives per class
         self.buffer = 1  # Number of buffer iterations. Interval at which the representatives will be updated
 
     def _create_loss(self, tensor_y: tf.Tensor, net_output: tf.Tensor):
@@ -40,15 +41,17 @@ class RepresentativesTrainer(Trainer):
     def _train_batch(self, sess, image_batch, target_batch, tensor_x: tf.Tensor, tensor_y: tf.Tensor,
                      train_step: tf.Operation, loss: tf.Tensor, increment: int, iteration: int, total_it: int):
         # Gets the representatives
-        rep_values, rep_labels = self.__get_representatives()
+        reps = self.__get_representatives()
 
         # Gets the respective weights
         weights_values = np.full((len(image_batch)), 1.0)
 
-        if len(rep_values) > 0:
-            rep_weights = np.full((len(rep_values)), 3.0)
-            weights_values = np.concatenate((weights_values, rep_weights))
+        if len(reps) > 0:
+            rep_weights = [rep.weight for rep in reps]
+            rep_values = [rep.value for rep in reps]
+            rep_labels = [rep.output for rep in reps]
             # Concatenates the training samples with the representatives
+            weights_values = np.concatenate((weights_values, rep_weights))
             image_batch = np.concatenate((image_batch, rep_values))
             target_batch = np.concatenate((target_batch, rep_labels))
 
@@ -68,18 +71,40 @@ class RepresentativesTrainer(Trainer):
         """
         Selects or retrieves the representatives from the data
 
-        :return: a tuple with 2 numpy.ndarray with the data and the labels. The data array has shape
-            **[n_representatives, x1, x2, ..., xn]** where [x1...xn] is the shape of a single sample image. The labels
-            array has shape **[n_representatives, n_labels]**.
-            The method returns a tuple of empty arrays **[]** if the number of representatives is less than the minimum
+        :return: a list of representatives.
+            The method returns an empty array **[]** if the number of representatives is less than the minimum
             number of representatives per batch (rep_per_batch)
         """
         repr_list = np.concatenate(self.representatives)
         if repr_list.size >= self.rep_per_batch:
             samples = np.random.choice(repr_list, size=self.rep_per_batch, replace=False)
-            return [i.value for i in samples], [i.output for i in samples]
+            return samples
         else:
-            return [], []
+            return []
+
+    def __modify_representatives(self, candidate_representatives):
+        """
+        Modifies the representatives list according to the new data
+        
+        :param candidate_representatives: the preselected representatives from the buffer
+        :return: None
+        """
+        for i, _ in enumerate(candidate_representatives):
+            nclass = int(np.argmax(candidate_representatives[i].output))
+            self.representatives[nclass].append(candidate_representatives[i])
+            self.class_count[nclass] += 1
+
+        # Recalculates the metrics (i.e. outputs and difs) of the representatives
+        self.__recalculate_metrics(self.representatives)
+        # Sorts representatives of each list, corresponding to each class
+        self.__calculate_crowd_distance(self.representatives)
+        for i in range(len(self.representatives)):
+            self.representatives[i].sort(key=lambda x: x.crowd_distance)
+            # self.representatives[i].sort(key=lambda x: x.metric * (1 + min(1, (total_it - x.iteration) / 10000)),
+            #                             reverse=True)
+            self.representatives[i] = self.representatives[i][-min(self.rep_per_class, len(self.representatives[i])):]
+
+        self.__recalculate_weights(self.representatives)
 
     def __buffer_samples(self, image_batch, target_batch, outputs, iteration):
         """
@@ -101,8 +126,9 @@ class RepresentativesTrainer(Trainer):
         outputs = outputs[sort_indices]
 
         for i in range(self.presel):
-            self.buffered_reps.append(Representative(image_batch[i], target_batch[i], difs[i], iteration,
-                                                     self.__extract_best_second_best(outputs[i], scores_ranking[i])))
+            self.buffered_reps.append(
+                Representative(image_batch[i].copy(), target_batch[i].copy(), difs[i].copy(), iteration,
+                               self.__extract_best_second_best(outputs[i].copy(), scores_ranking[i].copy())))
 
     def __clear_buffer(self):
         """
@@ -110,25 +136,6 @@ class RepresentativesTrainer(Trainer):
         :return: None
         """
         self.buffered_reps = []
-
-    def __modify_representatives(self, candidate_representatives):
-        """
-        Modifies the representatives list according to the new data
-        
-        :param candidate_representatives: the preselected representatives from the buffer
-        :return: None
-        """
-        for i, _ in enumerate(candidate_representatives):
-            nclass = int(np.argmax(candidate_representatives[i].output))
-            self.representatives[nclass].append(candidate_representatives[i])
-
-        # Sorts representatives of each list, corresponding to each class
-        self._calculate_crowd_distance(self.representatives)
-        for i in range(len(self.representatives)):
-            self.representatives[i].sort(key=lambda x: x.crowd_distance)
-            # self.representatives[i].sort(key=lambda x: x.metric * (1 + min(1, (total_it - x.iteration) / 10000)),
-            #                             reverse=True)
-            self.representatives[i] = self.representatives[i][-min(self.r, len(self.representatives[i])):]
 
     @staticmethod
     def __extract_best_second_best(scores, scores_ranking):
@@ -149,7 +156,8 @@ class RepresentativesTrainer(Trainer):
         return bvsb
 
     # TODO hacer más eficiente
-    def _calculate_crowd_distance(self, representatives):
+    @staticmethod
+    def __calculate_crowd_distance(representatives):
         """
         Calculates or recalculates the crowd distances for all the representatives
 
@@ -180,6 +188,40 @@ class RepresentativesTrainer(Trainer):
                     cls[j].crowd_distance += cls[j].reduced_rep[i][1] - cls[j - 1].reduced_rep[i][1]
                     cls[j].crowd_distance += cls[j + 1].reduced_rep[i][1] - cls[j].reduced_rep[i][1]
 
+    def __recalculate_weights(self, representatives):
+        """
+        Reassigns the weights of the representatives
+        :param representatives: a list of representatives
+        :return: None
+        """
+        total_count = np.sum(self.class_count)
+        # This version proposes that the total weight of representatives is calculated from the proportion of candidate
+        # representatives respect to the batch. E.g. a batch of 100 images and 10 are preselected, total_weight = 10
+        total_weight = (self.config.train_configurations[0].batch_size * 1.0) / self.presel
+        # The total_weight is adjusted to the proportion between candidate representatives and actual representatives
+        total_weight *= (total_count / (len(representatives) * self.rep_per_class))
+        probs = [count / total_count for count in self.class_count]
+        for i in range(len(representatives)):
+            for rep in representatives[i]:
+                # This version uses log as an stabilizer
+                rep.weight = math.log(probs[i] * total_weight)
+
+    def __recalculate_metrics(self, representatives):
+        """
+        Reassigns the metrics of the representatives
+        :param representatives: a list of representatives
+        :return: None
+        """
+        aux_rep = [rep for cls in representatives for rep in cls]
+        image_batch = np.array([rep.value for rep in aux_rep])
+        outputs = self.sess.run(self.model.get_output(), feed_dict={self.tensor_x: image_batch})
+        scores_ranking = np.argsort(outputs)  # Allows knowing the ranking of each class probability
+        outputs = np.sort(outputs)  # Order outputs over the last axis
+        difs = np.array([i[-1] - i[-2] for i in outputs])  # Best vs. Second Best
+        for i in range(len(aux_rep)):
+            aux_rep[i].metric = difs[i]
+            aux_rep[i].reduced_rep = self.__extract_best_second_best(outputs[i], scores_ranking[i])
+
 
 class Representative(object):
     """
@@ -202,3 +244,4 @@ class Representative(object):
         self.iteration = iteration
         self.reduced_rep = reduced_rep
         self.crowd_distance = crowd_distance
+        self.weight = 3.0
